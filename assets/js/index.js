e8f2d02c7201e4ea95dd31bbc41fd1ad0ffdb75c
l422720735 <422720735@qq.com>
2019-03-09 19:27:08 +0800

    广告

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,20 @@
 $(function () {
+
     $(".navList li").mouseover(function () {
         $(".navList li").removeClass("activeClass");
         $(".navList li").eq($(this).index()).addClass("activeClass");
+
     });
 
 
+    // 右上角点击我们把样式加上去
+    $('.listContent li .title .selectAd .title').mouseover(function () {
+        // 三角的旋转
+        $('.listContent li .title .selectAd .title .iconfont').removeClass("activeClass");
+        $('.listContent li .title .selectAd .title .iconfont').eq($(this).index()).addClass("activeClass");
 
-
+        // 框的宽度
+        $('.listContent li .title .theSelect').removeClass("activeClass");
+        $('.listContent li .title .theSelect').eq($(this).index()).addClass("activeClass");
+    })
 });
