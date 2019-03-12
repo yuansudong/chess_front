09952825b863b40396a007cd21ecb6ec39aa327c
l422720735 <422720735@qq.com>
2019-03-12 15:59:18 +0800

    发现页面

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,16 +20,7 @@ $(function () {
         $('.theSelect').hide();
     });
 
-    $(document).ready(function (e) {
-        //myApi1.JSON.lagout(v1,v2,v3)，
-        //v1,v2,v3是三个参数，其中
-        //v1是最外层的div
-        //v2是轮播图的播放速度，以毫秒为单位
-        //v3轮播图的最外层高与图片的高度差（控制点在图片外时，与图片的距离）
-        var myApi1 = new Myapi();
-        myApi1.JSON.lagout($('#banner'), 2000, 0);
 
-    });
 
 
     // 滚动页面
